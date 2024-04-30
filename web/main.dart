91a17597e30ac5e3e0109317b2c6a345d6bca057
Tassio Virginio <tassiovirginio@gmail.com>
2024-04-30 14:00:08 -0300

    implementando

--- web/main.dart
+++ web/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:web/helpers.dart';
 import 'package:teste01/DNose.dart';
 
@@ -6,6 +8,12 @@ void main() {
   final element = document.querySelector('#output') as HTMLDivElement;
   element.text = 'The time is ${now.hour}:${now.minute}'
       ' and your Dart web app is running!';
+
+
+  InputElement search = document.querySelector('#search') as InputElement;
+
+  var submit = document.querySelector('#submit') as ButtonElement;
+  // submit.click();
 
   // final element2 = document.querySelector('#msg') as HTMLDivElement;
   // element2.text = src;
