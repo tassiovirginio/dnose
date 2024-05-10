c876bf3e5bc45fbc633942c0d27b308a3b90b10e
Tassio Virginio <tassiovirginio@gmail.com>
2024-05-10 12:05:48 -0300

    implementando

--- bin/dnose.dart
+++ bin/dnose.dart
@@ -1,32 +1,27 @@
 import 'dart:io';
 
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf_plus/shelf_plus.dart';
 import 'package:dnose/main.dart';
-
-var headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Origin, Content-Type',
-};
 
 void main() => shelfRun(init);
 
 Handler init() {
   var app = Router().plus;
 
-  String current = "${Directory.current.path}/resultado.csv";
-  String current2 = "${Directory.current.path}/resultado2.csv";
+  String resultado = "${Directory.current.path}/resultado.csv";
+  String resultado2 = "${Directory.current.path}/resultado2.csv";
 
-  app.get('/download', () => File(current));
-  app.get('/download2', () => File(current2));
+  app.get('/download', () => File(resultado));
+  app.get('/download2', () => File(resultado2));
 
   app.get('/', () => File('public/index.html'));
 
   app.get('/processar', (Request request) async {
     String? path_project = request.url.queryParameters['path_project'];
     processar(path_project!);
-    return Response.ok("", headers: headers);
+    return Response.ok("Processamento concluído");
   });
 
-  return app.call;
+  return corsHeaders() >> app.call;
 }
