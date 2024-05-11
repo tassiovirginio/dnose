ccee4eea086a38cdd18a834ca8e00c72fc9df3f9
Tassio Virginio <tassiovirginio@gmail.com>
2024-05-11 19:35:25 -0300

    implementando

--- public/javascript.js
+++ public/javascript.js
@@ -1,9 +1,7 @@
 function carregarNomeProjeto(){
     const req3 = new XMLHttpRequest();
     req3.onload = (e) => {
-        console.log("=> " + req3.response);
         document.getElementById("projectname").innerHTML = req3.response;
-
     };
     req3.open("GET", "/projectnameatual", true);
     req3.send();
@@ -13,6 +11,7 @@ function carregarNomesTestSmells(){
     const req4 = new XMLHttpRequest();
     var nomes = new Array();
     var valores = new Array();
+    var valores2 = new Array();
     req4.onload = (e) => {
         var linhas = req4.response.split("\n");
         for (let i = 1; i < linhas.length; i++) {
@@ -21,28 +20,27 @@ function carregarNomesTestSmells(){
             if(nome != ""){
                 nomes.push(nome);
                 var valorLog = Math.log(valor);
-                valores.push(valorLog);
-                console.log(nome);
+                valores.push(valor);
+                valores2.push(valorLog);
             }
         }
-
-        carregarChart(nomes, valores);
+        carregarChart('myChart', nomes, valores, '# of Test Smells (log)');
+        carregarChart('myChart2', nomes, valores2, '# of Test Smells)');
     };
     req4.open("GET", "/charts_data", true);
     req4.send();
-    console.log(nomes);
     return nomes;
 }
 
-function carregarChart(nomes, valores){
-    const ctx = document.getElementById('myChart');
+function carregarChart(id, nomes, valores, msg){
+    const ctx = document.getElementById(id);
 
     new Chart(ctx, {
         type: 'bar',
         data: {
             labels: nomes,
             datasets: [{
-                label: '# of Test Smells (log)',
+                label: msg,
                 data: valores,
                 borderWidth: 1
             }]
@@ -59,24 +57,17 @@ function carregarChart(nomes, valores){
 
 window.onload = (event) => {
     const processando = document.getElementById("processando");
-    processando.innerHTML = "Processando: FALSE";
-
-    carregarNomeProjeto();
-
-    var nomes = carregarNomesTestSmells();
-
-    carregarResultados();
-
+    processando.innerHTML = "Processando: False";
     document.getElementById("loading").style.visibility = "hidden";
-
+    carregarNomeProjeto();
+    carregarNomesTestSmells();
+    carregarResultados();
     carregarSelectProjects();
-
 };
 
 function carregarResultados(){
     const req2 = new XMLHttpRequest();
     req2.onload = (e) => {
-        console.log("=> " + req2.response);
         if (req2.response === "true") {
             document.getElementById("resultado").style.visibility = "visible";
             document.getElementById("resultado2").style.visibility = "visible";
@@ -84,7 +75,6 @@ function carregarResultados(){
             document.getElementById("resultado").style.visibility = "hidden";
             document.getElementById("resultado2").style.visibility = "hidden";
         }
-
     };
     req2.open("GET", "/result1exists", true);
     req2.send();
@@ -93,11 +83,7 @@ function carregarResultados(){
 function carregarSelectProjects(){
     const req = new XMLHttpRequest();
     req.onload = (e) => {
-        console.log("=> " + req.response);
-
         var lista_projetos = JSON.parse(req.response);
-        console.log(lista_projetos);
-
         var select_projects = document.getElementById("select_project");
 
         for (var i = 0; i < lista_projetos.length; i++) {
@@ -118,13 +104,13 @@ function processar() {
     document.getElementById("loading").style.visibility = "visible";
 
     const processando = document.getElementById("processando");
-    processando.innerHTML = "Processando: TRUE";
+    processando.innerHTML = "Processando: True";
     document.getElementById("processando").className = "tag is-danger";
 
     const path = document.getElementById("select_project");
 
-    console.log(path.value);
     const req = new XMLHttpRequest();
+
     req.onload = (e) => {
         processando.innerHTML = "Processando: FALSE";
         const resultado = document.getElementById("resultado");
@@ -134,8 +120,6 @@ function processar() {
         document.getElementById("processando").className = "tag is-primary is-light";
 
         carregarNomeProjeto();
-
-        console.log("=> " + req.response);
     };
     req.open("GET", "/processar?path_project=" + path.value, true);
     req.send();
