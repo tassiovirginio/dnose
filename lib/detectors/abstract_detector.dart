b0c76fbde284bc36c191818bab3a20a77707e29e
Tassio Virginio <tassiovirginio@gmail.com>
2024-05-12 12:22:29 -0300

    implementando

--- lib/detectors/abstract_detector.dart
+++ lib/detectors/abstract_detector.dart
@@ -2,7 +2,7 @@ import 'package:dnose/models/test_class.dart';
 import 'package:dnose/models/test_smell.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 
-abstract class AbstractDetector {
+mixin AbstractDetector {
   String get testSmellName;
   List<TestSmell> detect(ExpressionStatement e, TestClass testClass, String testName);
 }
