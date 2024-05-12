92a6aac91aa2f9c40481b549fe0220b849758972
Tassio Virginio <tassiovirginio@gmail.com>
2024-05-12 12:55:40 -0300

    implementando

--- lib/detectors/assertion_roulette_detector.dart
+++ lib/detectors/assertion_roulette_detector.dart
@@ -3,14 +3,15 @@ import 'package:dnose/detectors/abstract_detector.dart';
 import 'package:dnose/models/test_smell.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 
-class AssertionRouletteDetector implements AbstractDetector{
+class AssertionRouletteDetector implements AbstractDetector {
   @override
   get testSmellName => "Assertion Roulette";
 
   List<TestSmell> testSmells = List.empty(growable: true);
 
   @override
-  List<TestSmell> detect(ExpressionStatement e, TestClass testClass, String testName) {
+  List<TestSmell> detect(
+      ExpressionStatement e, TestClass testClass, String testName) {
     _detect(e as AstNode, testClass, testName);
     return testSmells;
   }
@@ -21,15 +22,21 @@ class AssertionRouletteDetector implements AbstractDetector{
   void _detect(AstNode e, TestClass testClass, String testName) {
     if (e is SimpleStringLiteral &&
         e.parent is NamedExpression &&
-        e.parent!.parent!.parent!.childEntities.firstOrNull!.toString() == "expect"){
-      testSmells.add(TestSmell(testSmellName, testName, testClass, code: e.toSource(), start: testClass.lineNumber(e.offset), end: testClass.lineNumber(e.end)));
-    } else if (e is ArgumentList && e.parent is MethodInvocation && !e.toString().contains("reason:")
-    && e.parent!.childEntities.first.toString() == "expect") {
-      testSmells.add(TestSmell(
-          testSmellName, testName, testClass, code: e.toSource(),
+        e.parent!.parent!.parent!.childEntities.firstOrNull!.toString() ==
+            "expect") {
+      testSmells.add(TestSmell(testSmellName, testName, testClass,
+          code: e.toSource(),
           start: testClass.lineNumber(e.offset),
           end: testClass.lineNumber(e.end)));
-    }else {
+    } else if (e is ArgumentList &&
+        e.parent is MethodInvocation &&
+        !e.toString().contains("reason:") &&
+        e.parent!.childEntities.first.toString() == "expect") {
+      testSmells.add(TestSmell(testSmellName, testName, testClass,
+          code: e.toSource(),
+          start: testClass.lineNumber(e.offset),
+          end: testClass.lineNumber(e.end)));
+    } else {
       e.childEntities
           .whereType<AstNode>()
           .forEach((e) => _detect(e, testClass, testName));
