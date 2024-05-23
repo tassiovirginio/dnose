ff9e2299820e7a8c5eefc840937c7d6c8997ad94
Tássio Virgínio <tassiovirginio@gmail.com>
2024-05-23 09:41:17 -0300

    implementando

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,10 @@ void main(List<String> args) {
 }
 
 void processar(String pathProject) {
-  Logger.root.level = Level.ALL; // defaults to Level.INFO
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
+  Logger.root.level = Level.INFO; // defaults to Level.INFO
+  // Logger.root.onRecord.listen((record) {
+  //   print('${record.level.name}: ${record.time}: ${record.message}');
+  // });
 
   _logger.info("==============================================");
   _logger.info("========= Dart Test Smells Detector ==========");
