c5b031ea2c417f14cf215dda0ba1a323f2b67fa0
Tassio Virginio <tassiovirginio@gmail.com>
2024-07-27 16:10:17 -0300

    add metrics

--- lib/metrics/cyclomatic_complexity_metric.dart
+++ lib/metrics/cyclomatic_complexity_metric.dart
@@ -1,14 +1,14 @@
-
-
 import 'package:analyzer/src/dart/ast/ast.dart';
 import 'package:dnose/metrics/abstract_metric.dart';
 import 'package:dnose/models/test_class.dart';
 import 'package:dnose/models/test_metric.dart';
 
-class CyclomaticComplexityMetric implements AbstractMetric{
+//Base: https://github.com/bancolombia/dart-code-linter/blob/trunk/lib/src/analyzers/lint_analyzer/metrics/metrics_list/cyclomatic_complexity/cyclomatic_complexity_flow_visitor.dart
+class CyclomaticComplexityMetric implements AbstractMetric {
   @override
-  TestMetric calculate(ExpressionStatement e, TestClass testClass, String testName) {
-    _calculate(e,testClass,testName);
+  TestMetric calculate(
+      ExpressionStatement e, TestClass testClass, String testName) {
+    _calculate(e, testClass, testName);
 
     TestMetric testMetric = TestMetric(
         name: metricName,
@@ -25,8 +25,16 @@ class CyclomaticComplexityMetric implements AbstractMetric{
   int cont = 0;
 
   void _calculate(AstNode e, TestClass testClass, String testName) {
-    if (e is ForElement || e is IfElement || e is WhileStatement || e is SwitchStatement) {
-      cont ++;
+    if (e is AssertStatement ||
+    e is CatchClause ||
+    e is ConditionalExpression ||
+    e is ForStatement ||
+    e is IfStatement ||
+    e is SwitchCase ||
+    e is SwitchDefault ||
+    e is WhileStatement ||
+    e is YieldStatement) {
+      cont++;
     } else {
       e.childEntities
           .whereType<AstNode>()
@@ -36,5 +44,4 @@ class CyclomaticComplexityMetric implements AbstractMetric{
 
   @override
   String get metricName => "Cyclomatic Complexity";
-
 }
