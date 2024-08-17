a4eb561e10f63b6c58818e0435db31b6182fa42f
Tassio Virginio <tassiovirginio@gmail.com>
2024-08-17 14:30:16 -0300

    implementando testes do DNose

--- test/dnose_all_test.dart
+++ test/dnose_all_test.dart
@@ -8,15 +8,33 @@ import 'package:test/test.dart';
 void main() {
   DNose dnose = DNose();
 
-  test("Assertion Roulet Detect", () {
-    File file = File(
-        "${Directory.current.path}/test/assertion_roulette_test.dart");
+  test("Detect: Assertion Roulet", () {
+    File file =
+        File("${Directory.current.path}/test/samples/assertion_roulette_test.dart");
+
     if (file.path.endsWith("_test.dart") == true) {
       TestClass testClass = TestClass(
           commit: "", path: file.path, moduleAtual: "", projectName: "");
       var (testSmells, testMetrics) = dnose.scan(testClass);
-      var lista = testSmells.where((e) => e.name == "Assertion Roulette").toList();
-      expect(lista.length, 3, reason: "Deveria encontrar 3 test smells do tipo Assertion Roulette");
+      var lista =
+          testSmells.where((e) => e.name == "Assertion Roulette").toList();
+      expect(lista.length, 3,
+          reason: "Deveria encontrar 3 test smells do tipo Assertion Roulette");
+    }
+  });
+
+  test("Detect: Test Without Description", () {
+    File file =
+        File("${Directory.current.path}/test/samples/test_without_description_test.dart");
+
+    if (file.path.endsWith("_test.dart") == true) {
+      TestClass testClass = TestClass(
+          commit: "", path: file.path, moduleAtual: "", projectName: "");
+      var (testSmells, testMetrics) = dnose.scan(testClass);
+      var lista =
+          testSmells.where((e) => e.name == "Test Without Description").toList();
+      expect(lista.length, 3,
+          reason: "Deveria encontrar 3 test smells do tipo Test Without Description");
     }
   });
 }
---
+++ test/samples/test_without_description_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+void main() {
+  //Empty Description Test
+  test("", () => {});
+  test("", () {print("teste");});
+  test("", () => {if (true) {}});
+}
