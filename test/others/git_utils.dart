f3ef7dd9ebaced219a8f27f9e71041d97d6e0ab4
Tássio Virgínio <tassiovirginio@gmail.com>
2024-10-23 16:35:16 -0300

    iniciando o desenvolvimento do mining

---
+++ test/others/git_utils.dart
@@ -0,0 +1,29 @@
+import 'package:git/git.dart';
+import 'package:path/path.dart' as p;
+
+Future<void> main() async {
+  print('Current directory: ${p.current}');
+
+  if (await GitDir.isGitDir(p.current)) {
+    final gitDir = await GitDir.fromExisting(p.current);
+    final commitCount = await gitDir.commitCount();
+    print('Git commit count: $commitCount');
+
+    final latestCommit = await gitDir.currentBranch();
+    print('Latest commit: $latestCommit');
+
+    final currentBranch = await gitDir.currentBranch();
+    final name = currentBranch.branchName;
+    print('BranchName: $name');
+
+    final lista = await gitDir.branches();
+    lista.forEach((element) {
+      print(element.branchName);
+    });
+
+    final retorno = await gitDir.runCommand(['status']);
+    print(retorno.stdout);
+  } else {
+    print('Not a Git directory');
+  }
+}
