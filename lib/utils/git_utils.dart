dba2dca369f086484f786022d4970eb95944f123
Tássio Virgínio <tassiovirginio@gmail.com>
2024-10-30 15:24:33 -0300

    implementando funcao que retorna lista de arquivos que foram modificados em um commit

--- lib/utils/git_utils.dart
+++ lib/utils/git_utils.dart
@@ -2,7 +2,6 @@ import 'package:git/git.dart';
 import 'package:path/path.dart' as p;
 
 Future<void> main() async {
-
   var path = "/home/tassio/Desenvolvimento/repo.git/dnose";
 
   print('Current directory: ${path}');
@@ -28,46 +27,46 @@ Future<void> main() async {
     // print(retorno.stdout);
     final lista = await GitUtil.getListCommits(path);
 
-    lista.values.forEach((x) => print("${x.author}, ${x.message}"));
+    lista.values.forEach((x) {
+      print("${x.author}, ${x.message}");
+    });
 
-    final retorno_ = await GitUtil.getFileChangeCommit(path,"7282bb4489060a01a03923f0636933f773dd45b6");
+    final retorno_ = await GitUtil.getFileChangeCommit(
+        path, "7282bb4489060a01a03923f0636933f773dd45b6");
 
     print(retorno_);
-
   } else {
     print('Not a Git directory');
   }
 }
 
-class GitUtil{
-
-  static Future<String> getCurrentBranch(String path) async{
+class GitUtil {
+  static Future<String> getCurrentBranch(String path) async {
     final gitDir = await GitDir.fromExisting(path);
     final currentBranch = await gitDir.currentBranch();
     final name = currentBranch.branchName;
     return name;
   }
 
-  static Future<int> getSizeCommits(String path) async{
+  static Future<int> getSizeCommits(String path) async {
     final gitDir = await GitDir.fromExisting(path);
     final commitCount = await gitDir.commitCount();
     return commitCount;
   }
 
-  static Future<Map<String, Commit>> getListCommits(String path) async{
+  static Future<Map<String, Commit>> getListCommits(String path) async {
     final GitDir gitDir = await GitDir.fromExisting(path);
     final Map<String, Commit> mapa = await gitDir.commits();
     return mapa;
   }
 
-
-  static Future<List<String>> getFileChangeCommit(String path, String commit) async {
+  static Future<List<String>> getFileChangeCommit(
+      String path, String commit) async {
     final gitDir = await GitDir.fromExisting(path);
-    final retorno = await gitDir.runCommand(['show', '--name-only','--pretty=''', commit]);
+    final retorno = await gitDir
+        .runCommand(['show', '--name-only', '--pretty=' '', commit]);
     String files = retorno.stdout.toString();
     final lista = files.split('\n');
     return lista;
   }
-
 }
-
