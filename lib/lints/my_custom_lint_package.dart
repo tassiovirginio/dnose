aeae7cfaccfec0384c024d68391857bb088c71fd
Tássio Virgínio <tassiovirginio@gmail.com>
2024-12-17 14:30:12 -0300

    add example lint

---
+++ lib/lints/my_custom_lint_package.dart
@@ -0,0 +1,17 @@
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:dnose/lints/my_custom_lint_code.dart';
+
+// This is the entrypoint of our custom linter
+PluginBase createPlugin() => _ExampleLinter();
+
+/// A plugin class is used to list all the assists/lints defined by a plugin.
+class _ExampleLinter extends PluginBase {
+  /// We list all the custom warnings/infos/errors
+  @override
+  List<LintRule> getLintRules(CustomLintConfigs configs) => [
+        MyCustomLintCode(),
+      ];
+}
+
+
---
+++ lib/math/co-ocurrence.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'package:csv/csv.dart';
+
+void main() async {
+  // 1. Ler o arquivo CSV
+  final file = File('/home/tassio/testsmells_202411261748.csv');
+
+  final Map<String, List<String>> pathToSmells = {};
+
+  try {
+    // Lê o arquivo linha por linha de forma síncrona
+    List<String> lines = file.readAsLinesSync();
+
+    int cont = 0;
+
+    // Imprime cada linha do arquivo
+    for (var line in lines) {
+      // print(line);
+      if(cont > 0){
+        final path = line.split(",")[0];
+        final testsmell = line.split(",")[1];
+        pathToSmells.putIfAbsent(path, () => []).add(testsmell);
+      }else{
+        cont++;
+      }
+    }
+  } catch (e) {
+    print('Erro ao ler o arquivo: $e');
+  }
+
+
+  // print(pathToSmells);
+
+  // 3. Criar matriz de co-ocorrência
+  final allTestSmells = pathToSmells.values.expand((x) => x).toSet();
+  final Map<String, Map<String, int>> coOccurrence = {};
+
+  for (var smell1 in allTestSmells) {
+    coOccurrence[smell1] = {};
+    for (var smell2 in allTestSmells) {
+      coOccurrence[smell1]![smell2] = 0; // Inicializa com 0
+    }
+  }
+
+  for (var smells in pathToSmells.values) {
+    for (var smell1 in smells) {
+      for (var smell2 in smells) {
+        if (smell1 != smell2) {
+          coOccurrence[smell1]![smell2] = (coOccurrence[smell1]![smell2] ?? 0) + 1;
+        }
+      }
+    }
+  }
+
+  // 4. Imprimir matriz de co-ocorrência
+  print('Co-ocorrência de Test Smells:');
+  for (var row in coOccurrence.entries) {
+    print('${row.key}: ${row.value}');
+  }
+}
