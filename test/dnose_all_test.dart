9ac570089163eae2449542a4a5ba676859460ff4
Tássio Virgínio <tassiovirginio@gmail.com>
2025-01-23 13:05:47 -0300

    melhoria das detecções

--- test/dnose_all_test.dart
+++ test/dnose_all_test.dart
@@ -60,7 +60,7 @@ void main() {
   test("Detect: Duplicate Assert", () {
     verify(
         name: "Duplicate Assert",
-        qtd: 12,
+        qtd: 10,
         pathFile: "/test/samples/duplicate_assert_test.dart");
   });
 
--- test/samples/assertion_roulette_test.dart
+++ test/samples/assertion_roulette_test.dart
@@ -8,55 +8,55 @@ void main() {
     expect(1 + 2, 3);
   });
 
-  // test("AssertionRoulet", () {
-  //   // 0 
-  //   expect(1 + 2, 3, reason: "Verificando o valor");
-  //   expect(1 + 2, 3, reason: "Verificando o valor");
-  //   expect(1 + 2, 3, reason: "Verificando o valor");
-  // });
+  test("AssertionRoulet", () {
+    // 0 
+    expect(1 + 2, 3, reason: "Verificando o valor");
+    expect(1 + 2, 3, reason: "Verificando o valor");
+    expect(1 + 2, 3, reason: "Verificando o valor");
+  });
 
-  // test("AssertionRoulet", () {
-  //   // 1
-  //   expect(1 + 2, 3);
-  //   expect(1 + 2, 3,reason: "Teste"); //Melhorar a detecção para pegar esse tipo de erro
-  //   expect(1 + 2, 3);
-  // });
+  test("AssertionRoulet", () {
+    // 1
+    expect(1 + 2, 3);
+    expect(1 + 2, 3,reason: "Teste"); //Melhorar a detecção para pegar esse tipo de erro
+    expect(1 + 2, 3);
+  });
 
-  // test("AssertionRoulet", () {
-  //   // 0
-  //   expect(1 + 2, 3);
-  //   expect(1 + 2, 3, reason: "Teste"); //Melhorar a detecção para pegar esse tipo de erro
-  // });
+  test("AssertionRoulet", () {
+    // 0
+    expect(1 + 2, 3);
+    expect(1 + 2, 3, reason: "Teste"); //Melhorar a detecção para pegar esse tipo de erro
+  });
 
-  // test("AssertionRoulet", () {
-  //   // 1
-  //   expect(1 + 2, 3);
-  //   expect(1 + 2, 3);
-  // });
+  test("AssertionRoulet", () {
+    // 1
+    expect(1 + 2, 3);
+    expect(1 + 2, 3);
+  });
 
-  // test("AssertionRoulet", () {
-  //   // 2
-  //   expect(1 + 2, 3);
-  //   expect(1 + 2, 3);
-  //   expect(1 + 2, 3);
-  // });
+  test("AssertionRoulet", () {
+    // 2
+    expect(1 + 2, 3);
+    expect(1 + 2, 3);
+    expect(1 + 2, 3);
+  });
 
-  // test("AssertionRoulet", () {
-  //   // 0
-  //   expect(1 + 2, 3);
-  // });
+  test("AssertionRoulet", () {
+    // 0
+    expect(1 + 2, 3);
+  });
 
-  // soma(a, b) {
-  //   return a + b;
-  // }
+  soma(a, b) {
+    return a + b;
+  }
 
-  // test("Verificar Soma - 1 + 1", () {
-  //   var valor = soma(1, 1);
-  //   expect(valor, 2);
-  // });
+  test("Verificar Soma - 1 + 1", () {
+    var valor = soma(1, 1);
+    expect(valor, 2);
+  });
 
-  // test("Verificar Soma - 1 + 2", () {
-  //   var valor = soma(1, 2);
-  //   expect(valor, 3);
-  // });
+  test("Verificar Soma - 1 + 2", () {
+    var valor = soma(1, 2);
+    expect(valor, 3);
+  });
 }
