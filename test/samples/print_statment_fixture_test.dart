a709ce887573f35878e488840da09843b5afc42a
Tassio Virginio <tassiovirginio@gmail.com>
2025-04-13 09:36:34 -0300

    pequenas correções

--- test/samples/print_statment_fixture_test.dart
+++ test/samples/print_statment_fixture_test.dart
@@ -2,10 +2,16 @@ import 'package:process_run/stdio.dart';
 import 'package:test/test.dart';
 
 void main() {
-  var m = M();
-  var mm = M();
-  test("PrintStatmentFixture1", () => {m.print("teste1")});
-  test("PrintStatmentFixture2", () => {mm.prints("teste1")});
+  
+  test("PrintStatmentFixture1", () {
+    var m = M();
+    m.print("teste1");
+    expect((2+2), 4, reason: "Verificando o valor 123");
+    });
+  test("PrintStatmentFixture2", () {
+    var mm = M();
+    mm.prints("teste1");
+    });
   test("PrintStatmentFixture3", () => {print("teste1")});
   test("PrintStatmentFixture4", () => {prints("teste2")});
   test("PrintStatmentFixture5", () => {stdout.write("teste3")});
@@ -15,9 +21,9 @@ void main() {
 
 class M{
   void print(a){
-    print(a);
+    stdout.write('$a\n');
   }
   void prints(a){
-    print(a);
+    stdout.write('$a\n');
   }
 }
