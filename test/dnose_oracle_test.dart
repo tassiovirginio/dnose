463c870bccc11dcec5f3760da06c8606897a1900
Tássio Virgínio <tassiovirginio@gmail.com>
2025-04-29 10:43:21 -0300

    reformatando

--- test/dnose_oracle_test.dart
+++ test/dnose_oracle_test.dart
@@ -5,56 +5,71 @@ import 'package:dnose/models/test_class.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final dnose = DNoseCore();
+  final dnoseCore = DNoseCore();
   final path = Directory.current.path;
 
-  void verify(
-      {required String name, required int qtd, required String pathFile}) {
-    var (testSmells, testMetrics) =
-        dnose.scan(TestClass.test(File("$path$pathFile").path));
+  void verify({
+    required String name,
+    required int qtd,
+    required String pathFile,
+  }) {
+    var (testSmells, testMetrics) = dnoseCore.scan(
+      TestClass.test(File("$path$pathFile").path),
+    );
     var list = testSmells.where((e) => e.name == name).toList();
-    expect(list.length, qtd,
-        reason: "Deveria encontrar $qtd test smells $name");
+    expect(
+      list.length,
+      qtd,
+      reason: "Deveria encontrar $qtd test smells $name",
+    );
   }
 
   test("Detect: Assertion Roullet", () {
     verify(
-        name: "Assertion Roulette",
-        qtd: 25,
-        pathFile: "/test/oracle/assertion_roulette_test.dart_");
+      name: "Assertion Roulette",
+      qtd: 25,
+      pathFile: "/test/oracle/assertion_roulette_test.dart_",
+    );
   });
 
   test("Detect: Test Without Description", () {
     verify(
-        name: "Test Without Description",
-        qtd: 5,
-        pathFile: "/test/oracle/test_without_description_test.dart_");
+      name: "Test Without Description",
+      qtd: 5,
+      pathFile: "/test/oracle/test_without_description_test.dart_",
+    );
   });
 
   test("Detect: Empty Test", () {
     verify(
-        name: "Empty Test", qtd: 6, pathFile: "/test/oracle/empty_test.dart_");
+      name: "Empty Test",
+      qtd: 6,
+      pathFile: "/test/oracle/empty_test.dart_",
+    );
   });
 
   test("Detect: Unknown Test", () {
     verify(
-        name: "Unknown Test",
-        qtd: 8,
-        pathFile: "/test/oracle/unknown_test.dart_");
+      name: "Unknown Test",
+      qtd: 8,
+      pathFile: "/test/oracle/unknown_test.dart_",
+    );
   });
 
   test("Detect: Conditional Test Logic", () {
     verify(
-        name: "Conditional Test Logic",
-        qtd: 7,
-        pathFile: "/test/oracle/conditional_test_logic_test.dart_");
+      name: "Conditional Test Logic",
+      qtd: 7,
+      pathFile: "/test/oracle/conditional_test_logic_test.dart_",
+    );
   });
 
   test("Detect: Magic Number", () {
     verify(
-        name: "Magic Number",
-        qtd: 26,
-        pathFile: "/test/oracle/magic_number_test.dart_");
+      name: "Magic Number",
+      qtd: 26,
+      pathFile: "/test/oracle/magic_number_test.dart_",
+    );
   });
 
   // test("Detect: Duplicate Assert", () {
@@ -105,7 +120,6 @@ void main() {
   //       qtd: 1,
   //       pathFile: "/test/oracle/verbose_test.dart");
   // });
-
 
   // test("Detect: Sensitive Equality", () {
   //   verify(
