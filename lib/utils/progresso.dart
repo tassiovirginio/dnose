f6129498d09fc724b657d60eb7ce0fde7cc1cfc3
Tassio Virginio <tassiovirginio@gmail.com>
2025-05-02 15:42:40 -0300

    melhorias do shell

--- lib/utils/progresso.dart
+++ lib/utils/progresso.dart
@@ -2,33 +2,67 @@ import 'dart:io';
 
 class Progresso {
   static String _barra = '';
-  static int _total = 0;
+  static int _progresso = 0;
+  static int _tamanhoBarra = 50;
   static String project = '';
+  static bool _coresDisponiveis = true;
+  static bool _finalizado = false;
 
   static void setProject(String projeto) {
     project = projeto;
     _barra = '';
+    _progresso = 0;
+    _finalizado = false;
+    _coresDisponiveis = stdout.supportsAnsiEscapes;
   }
 
   static void adicionarBloco() {
-    if(_total > 100){
-      _total = 0;
-      // stdout.write('\n');
-      resetar();
-    }else{
-      _total += 1;
-      _barra += '█';
-      stdout.write('\r$project $_barra');
+    if (_finalizado) return;
+
+    _progresso += 1;
+
+    if (_progresso > 100) {
+      _progresso = 100;
+    }
+
+    final blocosCompletos = (_progresso * _tamanhoBarra) ~/ 100;
+    _barra = '█' * blocosCompletos;
+    final espacos = ' ' * (_tamanhoBarra - blocosCompletos);
+
+    final porcentagem = '${_progresso.toString().padLeft(3)}%';
+
+    if (_coresDisponiveis) {
+      stdout.write('\r\x1B[36m$project\x1B[0m [\x1B[32m$_barra\x1B[0m$espacos] $porcentagem');
+    } else {
+      stdout.write('\r$project [$_barra$espacos] $porcentagem');
     }
   }
 
   static void resetar() {
     _barra = '';
-    // print(''); // Nova linha
+    _progresso = 0;
+    _finalizado = false;
+    stdout.write('\r${' ' * (project.length + _tamanhoBarra + 10)}\r');
   }
 
   static void finalizado() {
-    _barra = '';
-    stdout.write('\r████████████████ Concluído ████████████████');
+    if (_finalizado) return;
+
+    // Completa a barra
+    _progresso = 100;
+    _barra = '█' * _tamanhoBarra;
+
+    // Limpa a linha atual
+    stdout.write('\r${' ' * (project.length + _tamanhoBarra + 10)}\r');
+
+    if (_coresDisponiveis) {
+      // Versão colorida
+      stdout.write('\r\x1B[32m✓ Processamento concluído com sucesso!\x1B[0m');
+    } else {
+      // Versão sem cores
+      stdout.write('\r✓ Processamento concluído com sucesso!');
+    }
+
+    _finalizado = true;
   }
 }
